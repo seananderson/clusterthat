53fd0ffa7dafeb19bbabc6ff858b486b6cbfc458
Colin Millar <colin.millar@ices.dk>
2019-08-29 15:20:00 -0700

    update haddock example data
    Former-commit-id: ad3602b78cde88bf05632d48a781393d340be051

--- data-raw/haddock-data.R
+++ data-raw/haddock-data.R
@@ -11,8 +11,8 @@ library(FLCore)
 library(FLa4a)
 load("data-raw/fits.rdata")
 
-haddock_ts <- clusterthat::fits2data_ts(fits)
-haddock_mod <- clusterthat::fits2data_mod(fits, fo, ffmsy, bbmsy)
+haddock_ts <- clusterthat::fits2data_ts(fits2)
+haddock_mod <- clusterthat::fits2data_mod(fits2, fo, ffmsy, bbmsy)
 
 usethis::use_data(haddock_mod, overwrite = TRUE)
 usethis::use_data(haddock_ts, overwrite = TRUE)
